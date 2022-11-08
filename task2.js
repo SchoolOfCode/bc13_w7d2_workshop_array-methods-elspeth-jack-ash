0bd91eabd5bfc9d518c127345362dd0c011f9022
Bazelden <jack.waller@hotmail.com>
2022-11-08 12:28:21 +0000

    Task 2 complete and additional array tested

--- task2.js
+++ task2.js
@@ -1,11 +1,26 @@
 let animals = [
-    "baboon",
-    "kangaroo",
-    "rhino",
-    "frog",
-    "beaver",
-    "horse",
-    "basalisk",
-  ];
-  
-let bListAnimals = // Your 'filter' code here.
+  "baboon",
+  "kangaroo",
+  "rhino",
+  "frog",
+  "beaver",
+  "horse",
+  "basalisk",
+];
+
+let people = ["bruce", "kameron", "billy", "jack", "ash", "elspeth", "bob"];
+
+function bObject(object) {
+  return object.startsWith("b");
+}
+
+let bListAnimals = animals.filter(function (animal) {
+  return animal.startsWith("b");
+});
+
+// let bListAnimals = animals.filter(bAnimal);
+
+let bPeople = people.filter(bObject);
+
+console.log(bListAnimals);
+console.log(bPeople);
