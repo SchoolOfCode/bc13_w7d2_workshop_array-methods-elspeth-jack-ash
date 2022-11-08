3e88472e9edee08609c244a76b6e0de46dbce23a
Ash <ashraf.ak.rahman@gmail.com>
2022-11-08 12:15:15 +0000

    finished task 1

--- task1.js
+++ task1.js
@@ -1,3 +1,6 @@
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+const capitalisedCats = cats.map(function (cat){
+    return cat.toUpperCase()
+})
+console.log(cats);    
